3a4621632dfd9cdf4772340b3fdaf0be51fbc7a5
Ian Zhang <yz4135@nyu.edu>
2020-04-20 00:38:46 -0400

    finally!

--- main.py
+++ main.py
@@ -244,14 +244,14 @@ def train():
         total_loss += raw_loss.data
         optimizer.param_groups[0]['lr'] = lr2
         
-        with open(args.save[:-3]+'.log','a') as log_file:
-            if batch == 100:
-                cur_loss = total_loss.item() / 100
+        if batch % args.log_interval == 0 and batch > 0:
+            with open(args.save[:-3]+'.log','a') as log_file:
+                cur_loss = total_loss.item() / args.log_interval
                 elapsed = time.time() - start_time
                 log_file.write('| epoch {:3d} | {:5d}/{:5d} batches | lr {:05.5f} | ms/batch {:5.2f} | '
                 'loss {:5.2f} | ppl {:8.2f} | bpc {:8.3f}\n'.format(
             epoch, batch, len(train_data) // args.bptt, optimizer.param_groups[0]['lr'],
-                            elapsed * 1000 / 100 , cur_loss, math.exp(cur_loss), cur_loss / math.log(2)))
+                            elapsed * 1000 / args.log_interval, cur_loss, math.exp(cur_loss), cur_loss / math.log(2)))
                 total_loss = 0
                 start_time = time.time()
             ###
