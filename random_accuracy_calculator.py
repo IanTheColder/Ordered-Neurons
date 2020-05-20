642d5ec4782a68ae83dadc490948b9c511011da9
Ian Zhang <yz4135@nyu.edu>
2020-05-19 23:15:44 -0400

    push random_accuracy_calculator.py to prince

--- random_accuracy_calculator.py
+++ random_accuracy_calculator.py
@@ -30,17 +30,40 @@ def getGold(filePath):
 def getPred(filePath):
     return [sent.rstrip('\n') for sent in open(filePath,'r').readlines()]
 
+getAll = True
+
+def accuracy(correct,total):
+    acc = {cons:0 for cons in total}
+    if getAll:
+        consList = total.keys()
+        getAll=False
+    for cons in correct:
+        acc[cons] = correct[cons]/total[cons]
+    return acc
+
+def mean(l):
+    return sum(l)/len(l)
+
+def meanStd(accs):
+    import statistics
+    result = {}
+    for cons in consList:
+        acc5 = [acc[cons] for acc in accs]
+        result[cons] = (mean(acc5),statistics.pstdev(acc5))
+
 if __name__=='__main__':
-    pred = getPred('random_MRG/random1.txt')
+    accs = []
     gold = getGold('treebank_3/parsed/mrg/WSJ')
     corpus_ref = {i: gold[i] for i in range(len(gold))}
-    corpus_sys = {i: pred[i] for i in range(len(pred))}
-    print(len(corpus_sys),len(corpus_ref))
-    print(corpus_sys[5])
-    correct, total = corpus_stats_labeled(corpus_sys, corpus_ref)
-    print(correct)
-    print(total)
+    for i in range(5):
+        pred = getPred('random_MRG/random{}.txt'.format(i))
+        corpus_sys = {i: pred[i] for i in range(len(pred))}
+        #print(len(corpus_sys),len(corpus_ref))
+        #print(corpus_sys[5])
+        correct, total = corpus_stats_labeled(corpus_sys, corpus_ref)
+        accs.append(accuracy(correct,total))
     
+    print(meanStd(accs))
     #output_trees = [tree.replace('\n','') for tree in sen_nltktrees]
     
 
